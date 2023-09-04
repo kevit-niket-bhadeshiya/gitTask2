319f2b6328f117dca59eb5a0a7ecd9faee6a6276
Niket Bhadeshiya <niket.bhadeshiya@kevit.io>
2023-09-04 13:37:56 +0530

    update logc to get users

--- index.js
+++ index.js
@@ -4,42 +4,43 @@ var app = express();
 
 // This responds with "Hello World" on the homepage
 app.get('/', function (req, res) {
-   console.log("Got a GET request for the homepage");
-   res.send('Hello GET');
+    console.log("Got a GET request for the homepage");
+    const userCount = 10;
+    res.send(`Hello GET. Users are: ${userCount}`);
 })
 
 
 // This responds a POST request for the homepage
 app.post('/', function (req, res) {
-   console.log("Got a POST request for the homepage");
-   res.send('Hello POST');
+    console.log("Got a POST request for the homepage");
+    res.send('Hello POST');
 })
 
 
 // This responds a DELETE request for the /del_user page.
 app.delete('/del_user', function (req, res) {
-   console.log("Got a DELETE request for /del_user");
-   res.send('Hello DELETE');
+    console.log("Got a DELETE request for /del_user");
+    res.send('Hello DELETE');
 })
 
 
 // This responds a GET request for the /list_user page.
 app.get('/list_user', function (req, res) {
-   console.log("Got a GET request for /list_user");
-   res.send('Page Listing');
+    console.log("Got a GET request for /list_user");
+    res.send('Page Listing');
 })
 
 
 // This responds a GET request for abcd, abxcd, ab123cd, and so on
-app.get('/ab*cd', function(req, res) {   
-   console.log("Got a GET request for /ab*cd");
-   res.send('Page Pattern Match');
+app.get('/ab*cd', function (req, res) {
+    console.log("Got a GET request for /ab*cd");
+    res.send('Page Pattern Match');
 })
 
 
 var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port);
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port);
 });
